8d4dbe299ca4655113926b15c780a6d0525670ed
GOWTHAM S <86189655+gowthams3@users.noreply.github.com>
2023-11-03 13:46:01 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,20 @@
+let OutputScreen = document.getElementById("outputscreen");
+
+function display(num){
+    OutputScreen.value += num;
+}
+function Calculate(){
+    try{
+        OutputScreen.value= eval(OutputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid")
+    }
+}
+function Clear(){
+    OutputScreen.value = " ";
+}
+function del(){
+    OutputScreen.value = OutputScreen.value.slice(0,-1);
+}
